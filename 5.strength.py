420fc82fb52ea89ae96571d72ef8f8fc8ff7ab7d
chingchenn <ching602504@gmail.com>
2021-09-09 10:43:44 +0800

    calculate rock rheology

--- 5.strength.py
+++ 5.strength.py
@@ -14,18 +14,18 @@ layerz = (0, 18e3, 30e3)   # 1st elem must be 0
 Dfc = ((2800,30,4e7),
        (2900,30,4e7),
        (3300,30,4e7))
-nAEs = ( (3.05, 1.25e-1, 2.76e+5),
-       (3.05, 1.25e-1, 3.76e+5),
-       (3.00, 7.00e+4, 5.20e+5))
+nAEs = ((3.05, 1.25e-1, 2.76e+5),
+       (3.05, 1.25e-1, 2.76e+5),
+       (3.00, 7.00e+4, 5.76e+5))
 edot = 1e-14  # high strain rate
-edot = 1e-15  # low strain rate
-deepz = layerz[-1] * 3
+edot = 1e-16  # low strain rate
+deepz = layerz[-1] * 4
 z = np.linspace(0, deepz, num=1000)
 
 #------------------------------------------------------------------------------
 # equation soluiton of plastic stress and viscosity
 frico_strength = f2.plastic_stress(z,layerz,Dfc)
-con_T = f2.continental_geothermal_T(z,20, 6,45)
+con_T = f2.continental_geothermal_T(z,22,6,45)
 visc = f2.visc_profile(z, con_T, edot, layerz, nAEs)
 visco_strength=visc* edot *2 #Pa
 #------------------------------------------------------------------------------
@@ -34,11 +34,16 @@ applied_strength = np.amin((visco_strength,frico_strength),axis=0)
 ax.plot(visco_strength/1e6,-z/1000,'--r',alpha=0.5)
 ax.plot(frico_strength/1e6,-z/1000,'--b',alpha=0.5)
 ax.plot(applied_strength/1e6,-z/1000,'k',lw=3)
-ax.set_ylim(-deepz/1000 +1,0)                                     
+ax.set_ylim(-120+1,0)                                     
 ax.set_xlim(0,1e3)
 ax.set_title('Rock Strength',fontsize=26)
 ax.set_xlabel('Strength (MPa)',fontsize=22)
 ax.set_ylabel('Depth (km)',fontsize=22)
 ax.grid()
-fig.savefig('/home/jiching/geoflac/figure/'+'strength_profile'+'.png')
+ax2=ax.twinx()
+temp=z/1000*0.6+con_T
+ax2.plot(temp,-z/1000,color='green')
+ax2.set_xlim(0,1500)
+ax2.set_ylim(-120+1,0)
+# fig.savefig('/home/jiching/geoflac/figure/'+'strength_profile'+'.png')
 # 
