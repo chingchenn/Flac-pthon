b6e9e4b30c49e23e9d5a5ee910ab9550bb1bb499
chingchenn <ching602504@gmail.com>
2021-12-15 18:23:28 +0800

    sxx and interface force

--- 13.interface_stress.py
+++ 13.interface_stress.py
@@ -11,11 +11,12 @@ import matplotlib.pyplot as plt
 
 model='s1303'
 path = 'D:/model/'+model+'/'
-path = '/home/jiching/geoflac/'+model+'/'
+# path = '/home/jiching/geoflac/'+model+'/'
 os.chdir(path)
 fl = flac.Flac();end = fl.nrec
 nex = fl.nx - 1;nez = fl.nz - 1
-force=np.zeros(end)
+force=np.zeros(end+1)
+forces=np.zeros(end+1)
 cmap = plt.cm.get_cmap('rainbow')
 fig, (ax) = plt.subplots(1,1,figsize=(10,12))
 for flame in range(1,end):
@@ -31,12 +32,19 @@ for flame in range(1,end):
     t = np.zeros(element_x[:,0].shape)
     t[:] = flame*0.2
     ff=np.zeros(nex)
-    for hh in range(nex):
+    crust_x = np.zeros(nex)
+    crust_z = np.zeros(nex)
+    for j in range(nex):
+        ind_oceanic = (strain_rate[j,:]>-12.6) 
+        if True in ind_oceanic:
+            for uu in range(nez):
+                height = z[j,uu+1]-z[j,uu]
+                forces[flame]+=height*sxx[j,uu]*1000/10e6
         for mm in range(nez+1):
-            ff[hh]+=sxx[hh,mm-1]*(z[hh,mm]-z[hh,mm-1])*1000/10e6
+            ff[j]+=sxx[j,mm-1]*(z[j,mm]-z[j,mm-1])*1000/10e6
     for kk in range(nez+1):
-        maxsxx_id=np.argmin(sxx[:,kk-1])
-        hight=z[maxsxx_id,kk]-z[maxsxx_id,kk-1]
+        maxsxx_id=np.argmin(strain_rate[:,kk-1])
+        hight=(z[maxsxx_id,kk]-z[maxsxx_id,kk-1])
         force[flame]+= sxx[maxsxx_id,kk-1]*hight*1000/10e6
     cb_plot =ax.scatter(element_x[:,0],t,c=ff,cmap=cmap)
 ax_cbin = fig.add_axes([0.93, 0.18, 0.23, 0.02])
@@ -44,8 +52,10 @@ cb = fig.colorbar(cb_plot,cax=ax_cbin,orientation='horizontal')
 ax_cbin.set_title('Stress')
 ax.set_xlim(0,1200)
 ax.set_ylim(0,50)
-fig.savefig('/home/jiching/geoflac/figure/'+model+'_sxx.jpg')
+# fig.savefig('/home/jiching/geoflac/figure/'+model+'_sxx.jpg')
 # plt.scatter(element_x,element_z,c=sxx,s=2.5)
 fig2, (ax2) = plt.subplots(1,1,figsize=(10,6))
-ax2.plot(fl.time,force,)
-fig2.savefig('/home/jiching/geoflac/figure/'+model+'_interface_force.jpg')
+# ax2.plot(fl.time,force,c='k')
+ax2.plot(fl.time,forces,c='g')
+ax2.set_xlim(0,50)
+# fig2.savefig('/home/jiching/geoflac/figure/'+model+'_interface_force.jpg')
