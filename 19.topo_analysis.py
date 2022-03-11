529b1191a615a508ed0c9d1b4509f702d2770987
chingchenn <ching602504@gmail.com>
2022-03-11 11:25:29 +0800

    add distance between arc and trench

--- 19.topo_analysis.py
+++ 19.topo_analysis.py
@@ -9,7 +9,6 @@ import matplotlib
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import function_savedata as fs
-from creat_database import trench, get_topo, nodes_to_elements
 
 model = sys.argv[1]
 #model = 'h0133'
@@ -18,70 +17,48 @@ path = '/home/jiching/geoflac/'+model+'/'
 #path = 'F:/model/'+model+'/'
 os.chdir(path)
     
-fl = flac.Flac()
-end = fl.nrec
-
+fl = flac.Flac();end = fl.nrec
 rainbow = cm.get_cmap('gray_r',end)    
 newcolors = rainbow(np.linspace(0, 1, end))
 cmap = plt.cm.get_cmap('gist_earth')
-zmax, zmin =10, -10
-xx2 = np.zeros(end) 
-zz2 = np.zeros(end)
-arc_x = np.zeros(end)
 savepath = '/home/jiching/geoflac/data/'
-trenchfile='/home/jiching/geoflac/'+'data/trench_for_'+model+'.csv'
-dis, time, topo = get_topo()
-if not os.path.exists(trenchfile):
-    print('No file')
-    trench_index,trench_x,trench_z = trench()
-    name='trench_for_'+model
-    fs.save_3array(name,savepath,time,trench_x,trench_z,'time','trench_x','trench_z')
-
-df = pd.read_csv(trenchfile)
-width = 600
-fig, (ax) = plt.subplots(1,1,figsize=(10,12))
-qqq=ax.scatter(dis,time,c=topo,cmap='gist_earth',vmax=6,vmin=-10)
-cbar=fig.colorbar(qqq,ax=ax)
-ax.plot(df.trench_x[df.trench_x>0],df.time[df.trench_x>0],c='k',lw=2)
-ax.set_xlim(0,dis[-1][-1])
-ax.set_ylim(0,fl.time[-1])
-ax.set_title(str(model)+" Bathymetry Evolution",fontsize=24)
-ax.set_ylabel('Time (Myr)',fontsize=20)
-ax.set_xlabel('Distance (km)',fontsize=20)
-cbar.set_label('Topography (km)',fontsize=20)
-fig.savefig('/home/jiching/geoflac/figure/'+model+'_topo.png')
-
-fig2, (ax2) = plt.subplots(1,1,figsize=(10,6))
-topo1 = 0; aa=0
-xmean = 0
-ictime = 20
+###==========================Time Series===============================
+distance = np.zeros(end)
+###======================== Stacked Topography ========================
+fig2, (ax2) = plt.subplots(1,1,figsize=(8,6))
+topo1 = 0;xmean = 0;ictime = 20
+width = 400
 for i in range(1,end):
     x, z = fl.read_mesh(i)
     xt = x[:,0]
     zt = z[:,0]
     t = np.zeros(xt.shape)
     t[:] = i*0.2
-    # print(xt[np.argmin(zt)])
-    arc_x[i] = xt[np.argmax(zt)]
-    x_mid = df.trench_x[i]
-    within_plot = (xt>x_mid-width) * (xt<x_mid+width)
+    x_trench = xt[np.argmin(zt)]
+    x_arc = xt[np.argmax(zt)]
+    z_trench = zt[np.argmin(zt)]
+    z_arc = zt[np.argmax(zt)]
+    within_plot = (xt>x_trench-width) * (xt<x_trench+width)
     if i >= end-ictime:
         topo1 += zt
-        xmean += (xt-x_mid)
-    ax2.plot(xt[within_plot]-x_mid,zt[within_plot],c=newcolors[i])
+        xmean += (xt-x_trench)
+    ax2.plot(xt[within_plot]-x_trench,zt[within_plot],c=newcolors[i]) # Stacked Topography
+    distance[i] = x_arc-x_trench 
+    if distance[i]>600 or distance[i]<0:
+        distance[i]=0
     
-
-#ind_within=(arc_x<900)*(trench_x>100)
-
 ax2.plot((xmean[within_plot]/ictime), (topo1[within_plot]/ictime),c='purple')
 ax2.set_xlim(-width,width)
 ax2.set_title(str(model)+"Topography")
-ax2.set_ylabel("Bathymetry (m)")
+ax2.set_ylabel("Bathymetry (km)")
 ax2.set_xlabel("Distance relative to trench (km)")
 fs.save_2txt(str(model)+'_stack_topography',savepath,xmean[within_plot]/ictime,topo1[within_plot]/ictime)
-
-#ax_cbin = fig.add_axes([0.67, 0.18, 0.23, 0.03])
-#cb_plot = ax.scatter([-1],[-1],s=0.1,c=[1],cmap=cmap,vmin=zmin, vmax=zmax)
-#cb = fig.colorbar(cb_plot,cax=ax_cbin,orientation='horizontal')
-#ax_cbin.set_title('Bathymetry (km)')
 fig2.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_topo_analysis.jpg')
+###================ distance between trench and arc peak ================
+fig3, (ax3) = plt.subplots(1,1,figsize=(10,6))
+ax3.plot(distance,fl.time,c='teal',lw=4)
+ax3.grid()
+ax3.set_ylim(0,fl.time[-1])
+fs.save_2txt(str(model)+'_dis_trench_arc',savepath,fl.time,distance)
+fig3.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_distance.jpg')
+
--- 19.topo_analysis_model.py
+++ 19.topo_analysis_model.py
@@ -11,16 +11,26 @@ import function_savedata as fs
 
 width=400
 fig2, (ax2) = plt.subplots(1,1,figsize=(8,6))
+fig3, (ax3) = plt.subplots(1,1,figsize=(8,6))
 model_list=['h0401','h0402','h0403','h0404']
 rainbow = cm.get_cmap('rainbow',len(model_list))
 newcolors = rainbow(np.linspace(0, 1, len(model_list)))
 for kk,model in enumerate(model_list):
     xmean,ztop=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_stack_topography.txt').T
     ax2.plot(xmean,ztop,c=newcolors[kk],label=model,lw=3)
+    time,distance=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_dis_trench_arc.txt').T
+    ax3.plot(distance,time,c=newcolors[kk],label=model,lw=3)
 
 ax2.set_xlim(-width,width)
 ax2.set_title("Topography comparation")
-ax2.set_ylabel("Bathymetry (m)")
-ax2.set_xlabel("Distance relative to trench (km)",fontsize=20)
+ax2.set_ylabel("Bathymetry (km)")
+ax2.set_xlabel("Distance relative to trench (km)",fontsize=16)
 ax2.legend(fontsize=16)
 fig2.savefig('/home/jiching/geoflac/figure'+'/'+'multi_topo_analysis.jpg')
+ax3.grid()
+ax3.legend(fontsize=16)
+ax3.set_ylim(0,time[-1])
+ax3.set_title("ATdis comparation")
+ax3.set_ylabel("Time (Ma)",fontsize=16)
+ax3.set_xlabel("Distance differences (km)",fontsize=16)
+fig3.savefig('/home/jiching/geoflac/figure'+'/'+'multi_distance.jpg')
