d950fe988dd2661b39ed7d9258dde04d90e1f776
chingchenn <ching602504@gmail.com>
2022-03-26 19:57:09 +0800

    add label

--- 10.stress_compare.py
+++ 10.stress_compare.py
@@ -14,7 +14,7 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 
 #model = sys.argv[1]
-model_list=['h0410','h0411','h0412','h0413','h0414']
+model_list=['chimex0601','chimex0602','chimex0603']
 rainbow = cm.get_cmap('rainbow',len(model_list))
 newcolors = rainbow(np.linspace(0, 1, len(model_list)))
 fig, (ax,ax2)= plt.subplots(2,1,figsize=(12,8))   
@@ -33,10 +33,12 @@ for kk,model in enumerate(model_list):
     ax4.plot(time,vl*31545741325,label=model,c=newcolors[kk],lw=2)
 
 ax.set_xlim(0,time[-1])
+ax.set_title('oceanic side force',fontsize=16)
 ax.tick_params(axis='x', labelsize=16)
 ax.tick_params(axis='y', labelsize=16)
 ax.grid()
 ax2.set_xlim(0,time[-1])
+ax2.set_title('continental side force',fontsize=16)
 ax2.tick_params(axis='x', labelsize=16)
 ax2.tick_params(axis='y', labelsize=16)
 ax2.grid()
@@ -47,6 +49,7 @@ ax3.tick_params(axis='y', labelsize=16)
 ax3.grid()
 ax3.legend()
 ax4.set_xlim(0,time[-1])
+ax4.set_title('oceanic side velocity',fontsize=16)
 ax4.tick_params(axis='x', labelsize=16)
 ax4.tick_params(axis='y', labelsize=16)
 ax4.grid()
@@ -54,6 +57,6 @@ ax4.set_xlabel('Time (Myr)',fontsize=16)
 ax4.set_ylabel('Velocity (mm/yr)',fontsize=16)
 ax4.legend()
 # ax2.plot([time[0],time[-1]],[0,0],'r--')
-fig.savefig('/home/jiching/geoflac/figure/forc.png')
-fig2.savefig('/home/jiching/geoflac/figure/ringforc.png')
-fig3.savefig('/home/jiching/geoflac/figure/vel.png')
+fig.savefig('/home/jiching/geoflac/figure/'+model_list[0]+'_'+model_list[-1]+'_forc.png')
+fig2.savefig('/home/jiching/geoflac/figure/'+model_list[0]+'_'+model_list[-1]+'_ringforc.png')
+fig3.savefig('/home/jiching/geoflac/figure/'+model_list[0]+'_'+model_list[-1]+'_vel.png')
