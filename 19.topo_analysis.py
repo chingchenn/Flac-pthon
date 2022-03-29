f7cc57443298a0dbf245df00c2b3c8bf6ebc3809
chingchenn <ching602504@gmail.com>
2022-03-29 23:33:36 +0800

    add others topography data including x and z dircetions

--- 19.topo_analysis.py
+++ 19.topo_analysis.py
@@ -11,8 +11,7 @@ import matplotlib.pyplot as plt
 import function_savedata as fs
 
 model = sys.argv[1]
-#model = 'h0133'
-#path = '/home/jiching/geoflac/'+model+'/'
+path = '/home/jiching/geoflac/'+model+'/'
 path='/scratch2/jiching/03model/'+model+'/'
 #path='/scratch2/jiching/22winter/'+model+'/'
 #path = 'F:/model/'+model+'/'
@@ -24,7 +23,15 @@ newcolors = rainbow(np.linspace(0, 1, end))
 cmap = plt.cm.get_cmap('gist_earth')
 savepath = '/home/jiching/geoflac/data/'
 ###==========================Time Series===============================
-distance = np.zeros(end)
+distance = np.zeros(end) #x
+xtrench = np.zeros(end) #x
+xarc = np.zeros(end) #x
+arc_basin_distance = np.zeros(end) #x
+trench_basin_distance = np.zeros(end) #x
+basin_width = np.zeros(end) #x
+basin_depth = np.zeros(end) #z
+arc_height = np.zeros(end) #z
+trench_height = np.zeros(end) #z
 ###======================== Stacked Topography ========================
 fig2, (ax2) = plt.subplots(1,1,figsize=(8,6))
 topo1 = 0;xmean = 0;ictime = 20
@@ -36,7 +43,9 @@ for i in range(1,end):
     t = np.zeros(xt.shape)
     t[:] = i*0.2
     x_trench = xt[np.argmin(zt)]
-    x_arc = xt[np.argmax(zt)]
+    xtrench[i]=x_trench
+    x_arc = xt[np.argmax(zt[xt<x_trench+200])]
+    xarc[i]=x_arc
     z_trench = zt[np.argmin(zt)]
     z_arc = zt[np.argmax(zt)]
     within_plot = (xt>x_trench-width) * (xt<x_trench+width)
@@ -47,6 +56,18 @@ for i in range(1,end):
     distance[i] = x_arc-x_trench 
     if distance[i]>600 or distance[i]<0:
         distance[i]=0
+    z_mid_basin = zt[np.argmin(zt[xt>x_arc])]
+    x_mid_basin = xt[np.argmin(zt[xt>x_arc])]
+    arc_basin_distance[i] = x_mid_basin-x_arc
+    trench_basin_distance[i] = x_mid_basin-x_trench
+    basin_depth[i] = z_mid_basin
+    zb = z_mid_basin/2
+    xbw = xt[zt<z_mid_basin]
+    if len(xbw)==0:
+        continue
+    basin_width[i]=(xbw[-1]-xbw[0])
+    arc_height[i] = z_arc
+    trench_height[i] = z_trench
     
 ax2.plot((xmean[within_plot]/ictime), (topo1[within_plot]/ictime),c='purple')
 ax2.set_xlim(-width,width)
@@ -56,9 +77,49 @@ ax2.set_xlabel("Distance relative to trench (km)")
 fs.save_2txt(str(model)+'_stack_topography',savepath,xmean[within_plot]/ictime,topo1[within_plot]/ictime)
 fig2.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_topo_analysis.jpg')
 ###================ distance between trench and arc peak ================
-fig3, (ax3) = plt.subplots(1,1,figsize=(10,6))
-ax3.plot(distance,fl.time,c='teal',lw=4)
+fig3, (ax3,ax10,ax11) = plt.subplots(3,1,figsize=(15,8))
+ax3.plot(fl.time,distance,c='teal',lw=2)
+ax10.plot(fl.time,xtrench,c='teal',lw=2)
+ax11.plot(fl.time,xarc,c='teal',lw=2)
 ax3.grid()
-ax3.set_ylim(0,fl.time[-1])
-fs.save_2txt(str(model)+'_dis_trench_arc',savepath,fl.time,distance)
+ax10.grid()
+ax11.grid()
+ax3.set_xlim(0,fl.time[-1])
+ax3.set_title('arc trench distance',fontsize=12)
+ax10.set_xlim(0,fl.time[-1])
+ax10.set_title('trench location (km)',fontsize=12)
+ax11.set_xlim(0,fl.time[-1])
+ax11.set_title('arc location (km)',fontsize=12)
+fs.save_4txt(str(model)+'_location_data',savepath,fl.time,distance,xtrench,xarc)
 fig3.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_distance.jpg')
+###============================== distance  ==============================
+fig4, (ax4,ax5,ax6) = plt.subplots(3,1,figsize=(15,8))
+ax4.plot(fl.time,arc_basin_distance,c='teal',lw=2)
+ax5.plot(fl.time,trench_basin_distance,c='teal',lw=2)
+ax6.plot(fl.time,basin_width,c='teal',lw=2)
+ax4.set_xlim(0,fl.time[-1])
+ax4.grid()
+ax4.set_title('arc basin distance',fontsize=12)
+ax5.set_xlim(0,fl.time[-1])
+ax5.grid()
+ax5.set_title('trench basin distance',fontsize=12)
+ax6.set_xlim(0,fl.time[-1])
+ax6.grid()
+ax6.set_title('basin width',fontsize=12)
+fs.save_4txt(str(model)+'_dis_data',savepath,fl.time,arc_basin_distance,trench_basin_distance,basin_width)
+fig4.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_xdirection.png')
+fig5,(ax7,ax8,ax9) = plt.subplots(3,1,figsize=(15,8))
+ax7.plot(fl.time,basin_depth,c='teal',lw=2)
+ax8.plot(fl.time,arc_height,c='teal',lw=2)
+ax9.plot(fl.time,trench_height,c='teal',lw=2)
+ax7.set_xlim(0,fl.time[-1])
+ax7.grid()
+ax7.set_title(model+'\n basin depth',fontsize=12)
+ax8.set_xlim(0,fl.time[-1])
+ax8.grid()
+ax8.set_title('arc height',fontsize=12)
+ax9.set_xlim(0,fl.time[-1])
+ax9.grid()
+ax9.set_title('trench height',fontsize=12)
+fs.save_4txt(str(model)+'_depth_data',savepath,fl.time,basin_depth,arc_height,trench_height)
+fig5.savefig('/home/jiching/geoflac/figure'+'/'+str(model)+'_zdirction.png')
--- 19.topo_analysis_model.py
+++ 19.topo_analysis_model.py
@@ -11,16 +11,29 @@ import function_savedata as fs
 
 width=700
 fig2, (ax2) = plt.subplots(1,1,figsize=(8,6))
-fig3, (ax3) = plt.subplots(1,1,figsize=(8,6))
-model_list=['h0504','h0503','h0502','h0501']
-#model_list=['chi0401','chi0402','chi0403','chi0404','chi0405','Chi01']
+fig3, (ax3,ax10,ax11) = plt.subplots(3,1,figsize=(15,8))
+fig4, (ax4,ax5,ax6) = plt.subplots(3,1,figsize=(15,8))
+fig5,(ax7,ax8,ax9) = plt.subplots(3,1,figsize=(15,8))
+#model_list=['chih0609','chih0613','chih0617']
+model_list=['chih0605','chih0614','chih0615','chih0616','chih0617']
 rainbow = cm.get_cmap('rainbow',len(model_list))
 newcolors = rainbow(np.linspace(0, 1, len(model_list)))
 for kk,model in enumerate(model_list):
     xmean,ztop=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_stack_topography.txt').T
     ax2.plot(xmean,ztop,c=newcolors[kk],label=model,lw=3)
-    time,distance=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_dis_trench_arc.txt').T
-    ax3.plot(distance,time,c=newcolors[kk],label=model,lw=3)
+    time,distance,xtrench,xarc=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_location_data.txt').T
+    time,arc_basin_distance,trench_basin_distance,basin_width=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_dis_data.txt').T
+    time,basin_depth,arc_height,trench_height=np.loadtxt('/home/jiching/geoflac/data/'+str(model)+'_depth_data.txt').T
+    ax3.plot(time,distance,lw=2,label=model,color=newcolors[kk])
+    ax10.plot(time,xtrench,lw=2,label=model,color=newcolors[kk])
+    ax11.plot(time,xarc,lw=2,label=model,color=newcolors[kk])
+    ax4.plot(time,arc_basin_distance,lw=2,label=model,color=newcolors[kk])
+    ax5.plot(time,trench_basin_distance,lw=2,label=model,color=newcolors[kk])
+    ax6.plot(time,basin_width,lw=2,label=model,color=newcolors[kk])
+    ax7.plot(time,basin_depth,lw=2,label=model,color=newcolors[kk])
+    ax8.plot(time,arc_height,lw=2,label=model,color=newcolors[kk])
+    ax9.plot(time,trench_height,lw=2,label=model,color=newcolors[kk])
+
 
 ax2.set_xlim(-width,width)
 ax2.set_title("Topography comparation")
@@ -34,4 +47,32 @@ ax3.set_ylim(0,time[-1])
 ax3.set_title("ATdis comparation")
 ax3.set_ylabel("Time (Ma)",fontsize=16)
 ax3.set_xlabel("Distance differences (km)",fontsize=16)
+ax10.grid()
+ax11.grid()
+ax3.set_xlim(0,time[-1])
+ax3.set_title('arc trench distance',fontsize=12)
+ax10.set_xlim(0,time[-1])
+ax10.set_title('trench location (km)',fontsize=12)
+ax11.set_xlim(0,time[-1])
+ax11.set_title('arc location (km)',fontsize=12)
 fig3.savefig('/home/jiching/geoflac/figure'+'/'+'multi_distance'+model_list[0]+'_'+model_list[-1]+'.jpg')
+ax4.set_xlim(0,time[-1])
+ax4.grid()
+ax4.set_title('arc basin distance',fontsize=12)
+ax5.set_xlim(0,time[-1])
+ax5.grid()
+ax5.set_title('trench basin distance',fontsize=12)
+ax6.set_xlim(0,time[-1])
+ax6.grid()
+ax6.set_title('basin width',fontsize=12)
+ax7.set_xlim(0,time[-1])
+ax7.grid()
+ax7.set_title(model+'\n basin depth',fontsize=12)
+ax8.set_xlim(0,time[-1])
+ax8.grid()
+ax8.set_title('arc height',fontsize=12)
+ax9.set_xlim(0,time[-1])
+ax9.grid()
+ax9.set_title('trench height',fontsize=12)
+fig4.savefig('/home/jiching/geoflac/figure'+'/'+'multi_distance'+model_list[0]+'_'+model_list[-1]+'_xdirection.png')
+fig5.savefig('/home/jiching/geoflac/figure'+'/'+'multi_distance'+model_list[0]+'_'+model_list[-1]+'_zdirction.png')
