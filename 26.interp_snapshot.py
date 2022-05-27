4adb301fd6ad10cd6d35ba907c289af94b4c7442
chingchenn <ching602504@gmail.com>
2022-05-27 22:07:01 +0800

    plot both phase&viscosity

--- 26.interp_snapshot.py
+++ 26.interp_snapshot.py
@@ -17,7 +17,7 @@ frame = int(sys.argv[2])
 
 path='/home/jiching/geoflac/'
 #path = '/scratch2/jiching/22winter/'
-#path = '/scratch2/jiching/03model/'
+path = '/scratch2/jiching/03model/'
 #path = 'F:/model/'
 # path = 'D:/model/'
 #path = '/Volumes/SSD500/model/'
@@ -29,16 +29,17 @@ os.chdir(path+model)
 fl = flac.Flac()
 x, z = fl.read_mesh(frame)
 
-phasein = 1
-tin     = 0
-vis     = 1
+phasein     = 1
+tin         = 0
+vis         = 1
+figure_plot = 0
 #-------------------------------------------------------------------
 # domain bounds
 left = -300
 right = 800
 up = 10
 down = -200
-dx = 1.5
+dx = 1.6
 dz = 0.6
 
 def find_trench_index(z):
@@ -125,7 +126,7 @@ f.close()
 ###############
 cpcmd = '''
 awk '{print $1,$2,$3+0}' %(phasefile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(phasefile)s.txt
-awk '{print $1,$2,$3+0}' %(tfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(tfile)s.txt
+#awk '{print $1,$2,$3+0}' %(tfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(tfile)s.txt
 awk '{print $1,$2,$3+0}' %(gfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(gfile)s.txt
 awk '{print $1,$2,$3+0}' %(visfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(visfile)s.txt
 #cp %(tfile)s  %(model)s_%(tfile)s.txt
@@ -135,40 +136,46 @@ mv  %(model)s_%(phasefile)s.txt %(model)s_%(gfile)s.txt  %(model)s_%(tfile)s.txt
 os.system(cpcmd)
 
 #------------------------------------------------------------------------------
-colors = ["#93CCB1","#550A35","#2554C7","#008B8B","#4CC552",
+if figure_plot:
+    def nodes_to_elements(xmesh,zmesh):
+        ele_x = (xmesh[:fl.nx-1,:fl.nz-1] + xmesh[1:,:fl.nz-1] + xmesh[1:,1:] + xmesh[:fl.nx-1,1:]) / 4.
+        ele_z = (zmesh[:fl.nx-1,:fl.nz-1] + zmesh[1:,:fl.nz-1] + zmesh[1:,1:] + zmesh[:fl.nx-1,1:]) / 4.
+        return ele_x, ele_z
+    colors = ["#93CCB1","#550A35","#2554C7","#008B8B","#4CC552",
           "#2E8B57","#524B52","#D14309","#ed45a7","#FF8C00",
           "#FF8C00","#455E45","#F9DB24","#c98f49","#525252",
           "#F67280","#00FF00","#FFFF00","#7158FF"]
-phase19= matplotlib.colors.ListedColormap(colors)
+    phase19= matplotlib.colors.ListedColormap(colors)
 
-fig, (ax)= plt.subplots(1,1,figsize=(20,6))
-cc = plt.cm.get_cmap('jet')
-#filepath = savepath+model+'_intp3-phase.'+str(frame)+'.txt'
-filepath = savepath+model+'_intp3-visc.'+str(frame)+'.txt'
-#x,z,ph=np.loadtxt(filepath).T
-x,z,vis=np.loadtxt(filepath).T
-#ax.scatter(x,z,c = ph,cmap = phase19,vmax=19,vmin=1,s=5)
-ax.scatter(x,-z,c=vis,cmap=cc,vmin=20, vmax=27,s=150)
-ax.set_aspect('equal')
-def nodes_to_elements(xmesh,zmesh):
-    ele_x = (xmesh[:fl.nx-1,:fl.nz-1] + xmesh[1:,:fl.nz-1] + xmesh[1:,1:] + xmesh[:fl.nx-1,1:]) / 4.
-    ele_z = (zmesh[:fl.nx-1,:fl.nz-1] + zmesh[1:,:fl.nz-1] + zmesh[1:,1:] + zmesh[:fl.nx-1,1:]) / 4.
-    return ele_x, ele_z
-x,z = fl.read_mesh(frame)
-temp = fl.read_temperature(frame)
-ax.contour(x,-z,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
-bwith = 3
-ax.spines['bottom'].set_linewidth(bwith)
-ax.spines['top'].set_linewidth(bwith)
-ax.spines['right'].set_linewidth(bwith)
-ax.spines['left'].set_linewidth(bwith)
-ax.tick_params(axis='x', labelsize=23)
-ax.tick_params(axis='y', labelsize=23)
-ymajor_ticks = np.linspace(200,0,num=5)
-ax.set_yticks(ymajor_ticks)
-xmajor_ticks = np.linspace(250,1000,num=6)
-ax.set_xticks(xmajor_ticks)
-ax.set_xlim(250,1000)
-ax.set_ylim(200,-30)
-fig.savefig('/home/jiching/geoflac/figure/'+model+'frame_'+str(frame)+'_interp_vis.png')
+    fig, (ax)= plt.subplots(2,1,figsize=(22,11))
+    cc = plt.cm.get_cmap('jet')
+    xt,zt = fl.read_mesh(frame)
+    temp = fl.read_temperature(frame)
+    bwith = 3
+    #--------------------- phase plotting -------------------------
+    filepath = savepath+model+'_intp3-phase.'+str(frame)+'.txt'
+    x,z,ph=np.loadtxt(filepath).T
+    ax[0].scatter(x,-z,c = ph,cmap = phase19,vmax=19,vmin=1,s=1)
+    #------------------- viscosity plotting -----------------------
+    filepath = savepath+model+'_intp3-visc.'+str(frame)+'.txt'
+    x,z,vis=np.loadtxt(filepath).T
+    ax[1].scatter(x,-z,c=vis,cmap=cc,vmin=20, vmax=27,s=1)
+    #---------------------- plot setting --------------------------
+    for qq in range(len(ax)):
+        ax[qq].set_aspect('equal')
+        ax[qq].contour(xt,-zt,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+        ax[qq].spines['bottom'].set_linewidth(bwith)
+        ax[qq].spines['top'].set_linewidth(bwith)
+        ax[qq].spines['right'].set_linewidth(bwith)
+        ax[qq].spines['left'].set_linewidth(bwith)
+        ax[qq].tick_params(axis='x', labelsize=23)
+        ax[qq].tick_params(axis='y', labelsize=23)
+        ymajor_ticks = np.linspace(200,0,num=5)
+        ax[qq].set_yticks(ymajor_ticks)
+        xmajor_ticks = np.linspace(250,1000,num=6)
+        ax[qq].set_xticks(xmajor_ticks)
+        ax[qq].set_xlim(250,1000)
+        ax[qq].set_ylim(200,-30)
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase&vis.png')
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase&vis.pdf')
 #--------------------------------------------------------------------------
