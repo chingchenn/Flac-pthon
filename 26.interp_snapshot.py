9eaf3ecaa520f073bc4be2212350f05889707a2c
chingchenn <ching602504@gmail.com>
2022-05-28 20:36:43 +0800

    add 4 figure

--- 26.interp_snapshot.py
+++ 26.interp_snapshot.py
@@ -12,27 +12,35 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 
 #-------------------------------------------------------------------
-model = sys.argv[1]
-frame = int(sys.argv[2])
-
+# model = sys.argv[1]
+# frame = int(sys.argv[2])
+model = 'ch1506'
+frame = 150
+plt.rcParams["font.family"] = "Times New Roman"
 path='/home/jiching/geoflac/'
 #path = '/scratch2/jiching/22winter/'
 path = '/scratch2/jiching/03model/'
 #path = 'F:/model/'
 # path = 'D:/model/'
-#path = '/Volumes/SSD500/model/'
+path = '/Volumes/SSD500/model/'
 savepath='/home/jiching/geoflac/data/'
-#savepath='/Volumes/SSD500/data/'
+savepath='/Volumes/SSD500/data/'
 figpath='/home/jiching/geoflac/figure/'
+figpath='/Users/ji-chingchen/OneDrive - 國立台灣大學/年會/2022/POSTER/'
 os.chdir(path+model)
 
 fl = flac.Flac()
 x, z = fl.read_mesh(frame)
 
-phasein     = 1
+phasein     = 0
 tin         = 0
-vis         = 1
+vis         = 0
+gravity     = 0
+cpp         = 0
 figure_plot = 0
+figure_plot1= 0
+figure_plot2= 0
+figure_plot3= 1
 #-------------------------------------------------------------------
 # domain bounds
 left = -300
@@ -110,44 +118,44 @@ if vis:
     f.write('%d %d\n' % vx.shape)
     flac.printing(vx, vz, visc, stream=f)
     f.close()
-
+if gravity:
 # get topography and gravity at uniform spacing
-px, topo, topomod, gravity = fg.compute_gravity2(frame)
-# convert to km and mGal before saving
-px *= 1e-3
-topo *= 1e-3
-topomod *= 1e-3
-gravity *= 1e5
-gfile = 'topo-grav.%d' % frame
-f = open(gfile, 'w')
-flac.printing(px, topo, gravity, topomod, stream=f)
-f.close()
+    px, topo, topomod, gravity = fg.compute_gravity2(frame)
+    # convert to km and mGal before saving
+    px *= 1e-3
+    topo *= 1e-3
+    topomod *= 1e-3
+    gravity *= 1e5
+    gfile = 'topo-grav.%d' % frame
+    f = open(gfile, 'w')
+    flac.printing(px, topo, gravity, topomod, stream=f)
+    f.close()
 
 ###############
-cpcmd = '''
-awk '{print $1,$2,$3+0}' %(phasefile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(phasefile)s.txt
-#awk '{print $1,$2,$3+0}' %(tfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(tfile)s.txt
-awk '{print $1,$2,$3+0}' %(gfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(gfile)s.txt
-awk '{print $1,$2,$3+0}' %(visfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(visfile)s.txt
-#cp %(tfile)s  %(model)s_%(tfile)s.txt
-#cp %(gfile)s  %(model)s_%(gfile)s.txt
-mv  %(model)s_%(phasefile)s.txt %(model)s_%(gfile)s.txt  %(model)s_%(tfile)s.txt %(model)s_%(visfile)s.txt /home/jiching/geoflac/data/.
+if cpp:
+    cpcmd = ''' awk '{print $1,$2,$3+0}' %(phasefile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(phasefile)s.txt
+    #awk '{print $1,$2,$3+0}' %(tfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(tfile)s.txt
+    #awk '{print $1,$2,$3+0}' %(gfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(gfile)s.txt
+    awk '{print $1,$2,$3+0}' %(visfile)s | awk '{if ($3>0) print $1,$2,$3}' > %(model)s_%(visfile)s.txt
+    #cp %(tfile)s  %(model)s_%(tfile)s.txt
+    #cp %(gfile)s  %(model)s_%(gfile)s.txt
+    mv  %(model)s_%(phasefile)s.txt %(model)s_%(gfile)s.txt  %(model)s_%(tfile)s.txt %(model)s_%(visfile)s.txt /home/jiching/geoflac/data/.
 ''' % locals()
-os.system(cpcmd)
+    os.system(cpcmd)
 
-#------------------------------------------------------------------------------
-if figure_plot:
-    def nodes_to_elements(xmesh,zmesh):
-        ele_x = (xmesh[:fl.nx-1,:fl.nz-1] + xmesh[1:,:fl.nz-1] + xmesh[1:,1:] + xmesh[:fl.nx-1,1:]) / 4.
-        ele_z = (zmesh[:fl.nx-1,:fl.nz-1] + zmesh[1:,:fl.nz-1] + zmesh[1:,1:] + zmesh[:fl.nx-1,1:]) / 4.
-        return ele_x, ele_z
-    colors = ["#93CCB1","#550A35","#2554C7","#008B8B","#4CC552",
+def nodes_to_elements(xmesh,zmesh):
+    ele_x = (xmesh[:fl.nx-1,:fl.nz-1] + xmesh[1:,:fl.nz-1] + xmesh[1:,1:] + xmesh[:fl.nx-1,1:]) / 4.
+    ele_z = (zmesh[:fl.nx-1,:fl.nz-1] + zmesh[1:,:fl.nz-1] + zmesh[1:,1:] + zmesh[:fl.nx-1,1:]) / 4.
+    return ele_x, ele_z
+colors = ["#93CCB1","#550A35","#2554C7","#008B8B","#4CC552",
           "#2E8B57","#524B52","#D14309","#ed45a7","#FF8C00",
           "#FF8C00","#455E45","#F9DB24","#c98f49","#525252",
           "#F67280","#00FF00","#FFFF00","#7158FF"]
-    phase19= matplotlib.colors.ListedColormap(colors)
-
-    fig, (ax)= plt.subplots(2,1,figsize=(22,11))
+phase19= matplotlib.colors.ListedColormap(colors)
+#------------------------------------------------------------------------------
+if figure_plot:
+    #fig, (ax)= plt.subplots(1,2,figsize=(27,5))
+    fig, (ax)= plt.subplots(1,2,figsize=(24,5))
     cc = plt.cm.get_cmap('jet')
     xt,zt = fl.read_mesh(frame)
     temp = fl.read_temperature(frame)
@@ -176,6 +184,103 @@ if figure_plot:
         ax[qq].set_xticks(xmajor_ticks)
         ax[qq].set_xlim(250,1000)
         ax[qq].set_ylim(200,-30)
+        xmajor_ticks = np.linspace(500,1000,num=6)
+        ax[qq].set_xticks(xmajor_ticks)
+        ax[qq].set_xlim(450,950)
     fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase&vis.png')
-    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase&vis.pdf')
+    # fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase&vis.pdf')
 #--------------------------------------------------------------------------
+if figure_plot1:
+    fig, (ax)= plt.subplots(1,1,figsize=(13,5))
+    cc = plt.cm.get_cmap('jet')
+    xt,zt = fl.read_mesh(frame)
+    temp = fl.read_temperature(frame)
+    bwith = 3
+    #------------------- viscosity plotting -----------------------
+    filepath = savepath+model+'_intp3-visc.'+str(frame)+'.txt'
+    x,z,vis=np.loadtxt(filepath).T
+    ax.scatter(x,-z,c=vis,cmap=cc,vmin=20, vmax=27,s=1)
+    #---------------------- plot setting --------------------------
+    ax.set_aspect('equal')
+    ax.contour(xt,-zt,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+    ax.spines['bottom'].set_linewidth(bwith)
+    ax.spines['top'].set_linewidth(bwith)
+    ax.spines['right'].set_linewidth(bwith)
+    ax.spines['left'].set_linewidth(bwith)
+    ax.tick_params(axis='x', labelsize=23)
+    ax.tick_params(axis='y', labelsize=23)
+    ymajor_ticks = np.linspace(200,0,num=5)
+    ax.set_yticks(ymajor_ticks)
+    #xmajor_ticks = np.linspace(250,1000,num=6)
+    #ax.set_xticks(xmajor_ticks)
+    #ax.set_xlim(250,1000)
+    ax.set_ylim(200,-30)
+    xmajor_ticks = np.linspace(500,1000,num=6)
+    ax.set_xticks(xmajor_ticks)
+    ax.set_xlim(450,950)
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_vis.png')
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_vis.pdf')
+if figure_plot2:
+    fig, (ax)= plt.subplots(1,1,figsize=(13,5))
+    cc = plt.cm.get_cmap('jet')
+    xt,zt = fl.read_mesh(frame)
+    temp = fl.read_temperature(frame)
+    bwith = 3
+    #--------------------- phase plotting -------------------------
+    filepath = savepath+model+'_intp3-phase.'+str(frame)+'.txt'
+    x,z,ph=np.loadtxt(filepath).T
+    ax.scatter(x,-z,c=ph,cmap=phase19,vmin=1, vmax=19,s=1)
+    #---------------------- plot setting --------------------------
+    ax.set_aspect('equal')
+    ax.contour(xt,-zt,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+    ax.spines['bottom'].set_linewidth(bwith)
+    ax.spines['top'].set_linewidth(bwith)
+    ax.spines['right'].set_linewidth(bwith)
+    ax.spines['left'].set_linewidth(bwith)
+    ax.tick_params(axis='x', labelsize=23)
+    ax.tick_params(axis='y', labelsize=23)
+    ymajor_ticks = np.linspace(200,0,num=5)
+    ax.set_yticks(ymajor_ticks)
+    #xmajor_ticks = np.linspace(250,1000,num=6)
+    #ax.set_xticks(xmajor_ticks)
+    #ax.set_xlim(250,1000)
+    ax.set_ylim(200,-30)
+    xmajor_ticks = np.linspace(500,1000,num=6)
+    ax.set_xticks(xmajor_ticks)
+    ax.set_xlim(450,950)
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase.png')
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_phase.pdf')
+if figure_plot3:
+    fig, (ax)= plt.subplots(4,1,figsize=(13,17))
+    cc = plt.cm.get_cmap('jet')
+    bwith = 3
+    frame_list=[30,60,90,150]
+    #--------------------- viscosity plotting -------------------------
+    for qq,frame in enumerate(frame_list):
+        xt,zt = fl.read_mesh(frame)
+        temp = fl.read_temperature(frame)    
+        filepath = savepath+model+'_intp3-visc.'+str(frame)+'.txt'
+        x,z,vis=np.loadtxt(filepath).T
+        ax[qq].scatter(x,-z,c=vis,cmap=cc,vmin=20, vmax=27,s=1)
+    #---------------------- plot setting --------------------------
+        ax[qq].set_aspect('equal')
+        ax[qq].contour(xt,-zt,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+        ax[qq].spines['bottom'].set_linewidth(bwith)
+        ax[qq].spines['top'].set_linewidth(bwith)
+        ax[qq].spines['right'].set_linewidth(bwith)
+        ax[qq].spines['left'].set_linewidth(bwith)
+        ax[qq].tick_params(axis='x', labelsize=23)
+        ax[qq].tick_params(axis='y', labelsize=23)
+        ymajor_ticks = np.linspace(200,0,num=5)
+        ax[qq].set_yticks(ymajor_ticks)
+        xmajor_ticks = np.linspace(250,1000,num=6)
+        ax[qq].set_xticks(xmajor_ticks)
+        ax[qq].set_xlim(250,1000)
+        ax[qq].set_ylim(200,-30)
+        # xmajor_ticks = np.linspace(500,1000,num=6)
+        # ax[qq].set_xticks(xmajor_ticks)
+        # ax[qq].set_xlim(450,950)
+        if qq != 3:
+            ax[qq].axes.xaxis.set_visible(False)
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_vis_all.png')
+    fig.savefig(figpath+model+'frame_'+str(frame)+'_interp_vis_all.pdf')
