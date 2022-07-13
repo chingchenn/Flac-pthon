ac95b7864c80df60cdb3761375d0b9d346644c6b
chingchenn <ching602504@gmail.com>
2022-07-13 13:34:55 +0800

    interplotate phase and viscosity

--- 30.run_interpolate.py
+++ 30.run_interpolate.py
@@ -10,33 +10,38 @@ import sys, os
 import numpy as np
 import flac
 import function_for_flac as fd
+import function_savedata as fs
 import matplotlib.pyplot as plt
 from scipy import interpolate
 
 
 #-------------------------------------------------------------------
 model = sys.argv[1]
-frame = int(sys.argv[2])
-#model = 'cH1404'
-#frame = 120
-plt.rcParams["font.family"] = "Times New Roman"
+#frame = int(sys.argv[2])
+frame = 120
 path='/home/jiching/geoflac/'
-#path = '/scratch2/jiching/22winter/'
-#path = '/scratch2/jiching/03model/'
-#path = 'F:/model/'
-# path = 'D:/model/'
-#path = '/Volumes/SSD500/model/'
 savepath='/home/jiching/geoflac/data/'
-#savepath='/Volumes/SSD500/data/'
 figpath='/home/jiching/geoflac/figure/'
-#figpath='/Users/ji-chingchen/OneDrive - 國立台灣大學/年會/2022/POSTER/'
 os.chdir(path+model)
 
 fl = flac.Flac()
-frame = 120
-grid_x, grid_z = fd.make_grid(0, 1200, 0, 300, 0.5, 0.5)
-values = fl.read_sxx(frame)
+
+
+padding = 30
+grid_x, grid_z = fd.make_grid(0-padding, 1200+padding, -300-padding, 0+padding, 0.5, 0.5)
+values = fl.read_visc(frame)
 x, z= fl.read_mesh(frame)
 ele_x,ele_z=flac.elem_coord(x, z)
 points = np.vstack((ele_x.flat, ele_z.flat)).T
-grid_z1 = interpolate.griddata(points, values, (grid_x, grid_z), method='linear')
+grid_z1 = interpolate.griddata(points, values.flatten(), (grid_x, grid_z), method='linear')
+fs.save_3txt(model+'_frame_'+str(frame)+'interpolate_visc','/home/jiching/geoflac/data/',
+    grid_x[~np.isnan(grid_z1)],grid_z[~np.isnan(grid_z1)],grid_z1[~np.isnan(grid_z1)])
+
+mx, mz, mage, mphase, idm, a1, a2, ntriag = fl.read_markers(frame)
+points = np.vstack((mx.flat, mz.flat)).T
+f0 = interpolate.griddata(points, mphase.flatten(), (grid_x, grid_z), method='nearest')
+f0=f0.astype(np.float32)
+f = fd.clip_topo(grid_x, grid_z, f0, x, z)
+fs.save_3txt(model+'_frame_'+str(frame)+'interpolate_ph','/home/jiching/geoflac/data/',
+    grid_x[~np.isnan(f)],grid_z[~np.isnan(f)],f0[~np.isnan(f)])
+
