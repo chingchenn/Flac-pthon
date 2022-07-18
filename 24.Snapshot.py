4bfb552d0e285046df7cf11e2bc8d6a9fb568e2c
chingchenn <ching602504@gmail.com>
2022-07-18 11:12:30 +0800

    add Vx

--- 24.Snapshot.py
+++ 24.Snapshot.py
@@ -19,7 +19,7 @@ import function_savedata as fs
 import function_for_flac as fd
 import matplotlib.pyplot as plt
 import Main_snapshot as Ms
-plt.rcParams["font.family"] = "Times New Roman"
+# plt.rcParams["font.family"] = "Times New Roman"
 #---------------------------------- SETTING -----------------------------------
 path = '/home/jiching/geoflac/'
 #path = '/scratch2/jiching/22winter/'
@@ -34,7 +34,9 @@ model = sys.argv[1]
 os.chdir(path+model)
 fl = flac.Flac();end = fl.nrec
 time=fl.time
-plotting_png = 1
+plotting_png = 0
+plotting_vx = 0
+plotting_vz = 0
 gif = 1
 mp4 = 1
 labelsize = 26
@@ -56,20 +58,10 @@ if plotting_png:
         #ax.scatter(ele_x,-ele_z,c = phase,cmap = phase19,vmax=19,vmin=1,s=150)
         ax.scatter(xm,-zm,c = ph,cmap = phase19,vmax=19,vmin=1,s=5)
         ax.contour(x,-z,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
-    
     #--------------------------------------------------------------------------
         ele_x,ele_z,vis,ztop = Ms.get_vis(i)
         cc = plt.cm.get_cmap('jet')
         cb_plot=ax2.scatter(ele_x,-ele_z,c=vis,cmap=cc,vmin=20, vmax=27,s=150)
-    # ax_cbin = fig.add_axes([0.63, 0.35, 0.23, 0.03])
-    # cb = fig.colorbar(cb_plot,cax=ax_cbin,orientation='horizontal')
-    # ax_cbin.set_title('Pa s',fontsize=20)
-    
-    # ax2.set_title('Viscous '+str(model)+' at '+str(round(fl.time[i-1],1))+' Myr',fontsize=24)
-    # ax.set_ylabel('Depth (km)',fontsize=20)
-    # ax.set_xlabel('Distance (km)',fontsize=20)
-    # ax2.set_ylabel('Depth (km)',fontsize=20)
-    # ax2.set_xlabel('Distance (km)',fontsize=20)
         ax.set_aspect('equal')
         ax2.set_aspect('equal')
      
@@ -106,7 +98,68 @@ if plotting_png:
         fig.savefig(path+model+'/phase_vis/frame_'+qq+'_phase_vis.png')
         fig.gca()
         plt.close(fig)
-
+if plotting_vx:
+    for i in range(1,end):
+        fig,(ax)=plt.subplots(1,1,figsize=(12,8))
+        cc = plt.cm.get_cmap('BrBG')
+        x,z, = fl.read_mesh(i)
+        x,z,ele_x,ele_z,phase,temp,ztop=Ms.plot_snapshot(i)
+        vx,vz = fl.read_vel(i) # cm/yr
+        ref_vx = vx[370,39]
+        cb_plot = ax.scatter(x,z,c=(vx-ref_vx),cmap = cc,vmin = -3, vmax = 3)
+        ax_cbin = fig.add_axes([0.2,0.4,0.15,0.01])
+        cb = fig.colorbar(cb_plot,cax = ax_cbin,orientation = 'horizontal')
+        ax.contour(x,z,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+        ax.set_ylabel('Depth (km)',fontsize=20)
+        ax.set_xlabel('Distance (km)',fontsize=20)
+        ax_cbin.set_title('cm/yr',fontsize=20)
+        ax.set_aspect('equal')
+        ax.set_xlim(0,1200)
+        ax.set_ylim(-300,20)
+        bwith = 3
+        ax.spines['bottom'].set_linewidth(bwith)
+        ax.spines['top'].set_linewidth(bwith)
+        ax.spines['right'].set_linewidth(bwith)
+        ax.spines['left'].set_linewidth(bwith)
+        if i < 10:
+            qq = '00'+str(i)
+        elif i < 100 and i >=10:
+            qq = '0'+str(i)
+        else:
+            qq=str(i)
+        ax.set_title('horizontal velocity (cm/yr) '+str(model)+' at '+str(round(fl.time[i-1],1))+' Myr',fontsize=24)
+        fig.savefig(path+model+'/phase_vis/frame_'+qq+'_Vx.png')
+if plotting_vz:
+    for i in range(1,end):
+        fig,(ax)=plt.subplots(1,1,figsize=(12,8))
+        cc = plt.cm.get_cmap('BrBG')
+        x,z, = fl.read_mesh(i)
+        x,z,ele_x,ele_z,phase,temp,ztop=Ms.plot_snapshot(i)
+        vx,vz = fl.read_vel(i) # cm/yr
+        ref_vz = vz[370,39]
+        cb_plot = ax.scatter(x,z,c=(vz-ref_vz),cmap = cc,vmin = -5, vmax = 5)
+        ax_cbin = fig.add_axes([0.2,0.4,0.15,0.01])
+        cb = fig.colorbar(cb_plot,cax = ax_cbin,orientation = 'horizontal')
+        ax.contour(x,z,temp,cmap='rainbow',levels =[0,200,400,600,800,1000,1200],linewidths=3)
+        ax.set_ylabel('Depth (km)',fontsize=20)
+        ax.set_xlabel('Distance (km)',fontsize=20)
+        ax_cbin.set_title('cm/yr',fontsize=20)
+        ax.set_aspect('equal')
+        ax.set_xlim(0,1200)
+        ax.set_ylim(-300,20)
+        bwith = 3
+        ax.spines['bottom'].set_linewidth(bwith)
+        ax.spines['top'].set_linewidth(bwith)
+        ax.spines['right'].set_linewidth(bwith)
+        ax.spines['left'].set_linewidth(bwith)
+        if i < 10:
+            qq = '00'+str(i)
+        elif i < 100 and i >=10:
+            qq = '0'+str(i)
+        else:
+            qq=str(i)
+        ax.set_title('vertical velocity (cm/yr) '+str(model)+' at '+str(round(fl.time[i-1],1))+' Myr',fontsize=24)
+        fig.savefig(path+model+'/phase_vis/frame_'+qq+'_Vz.png')
 #-----------------------------creat GIF-----------------------------------------
 if gif: 
     from PIL import Image
@@ -114,7 +167,6 @@ if gif:
      
     # Create the frames
     frames = []
-    imgs = glob.glob(path+model+"/phase_vis/*_phase_vis.png")
     for i in  range(1,end):
         if i < 10:
             qq = '00'+str(i)
@@ -127,12 +179,12 @@ if gif:
         frames.append(new_frame)
      
     # Save into a GIF file that loops forever
-    frames[0].save(path+model+'/phase_vis/png_to_gif.gif', format='GIF', append_images=frames[1:], 
+    frames[0].save(path+model+'/phase_vis/phase_vis.gif', format='GIF', append_images=frames[1:], 
                    save_all=True, duration=80, loop=0)
     
 #-----------------------------creat mp4-----------------------------------------    
 if mp4:
     import moviepy.editor as mp
-    clip = mp.VideoFileClip(path+model+"/phase_vis/png_to_gif.gif")
-    clip.write_videofile(figpath+'phase_vis_'+model+".mp4")
-    
+    clip = mp.VideoFileClip(path+model+"/phase_vis/phase_vis.gif")
+    #clip.write_videofile(figpath+'phase_vis_'+model+".mp4")
+    clip.write_videofile(figpath+'Vz_'+model+".mp4")
