45d393e16fc6f47125c692a67b1570a7771e35cd
chingchenn <ching602504@gmail.com>
2022-11-07 01:58:43 +0800

    magma area

--- 28.cal_LVC.py
+++ 28.cal_LVC.py
@@ -11,22 +11,23 @@ import numpy as np
 import flac
 import flacmarker2vtk
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import function_for_flac as fd
 import matplotlib.pyplot as plt
 
 
 path = '/home/jiching/geoflac/'
 #path = '/scratch2/jiching/22winter/'
-path = '/scratch2/jiching/03model/'
-#path = 'F:/model/'
-#path = 'D:/model/'
-#path = '/Volumes/SSD500/model/'
-#path='/Users/ji-chingchen/Desktop/model/'
+#path = '/scratch2/jiching/03model/'
+path = '/scratch2/jiching/04model/'
+#path ='/Volumes/My Book/model/03model/'
+#path = '/Users/chingchen/Desktop/model/'
 savepath='/home/jiching/geoflac/data/'
+#savepath='/Users/chingchen/Desktop/data/'
 figpath='/home/jiching/geoflac/figure/'
+#figpath='/Users/chingchen/Desktop/figure/'
 
-model = 'ch1528'
+model = 'Ref_Nazca'
 os.chdir(path+model)
 fl = flac.Flac()
 time=fl.time
@@ -50,7 +51,70 @@ model_list=['ch1519','ch1522','ch1406','ch1512','ch1513','ch1510',
 model_list=['ch1522','ch1512','ch1513','ch1510','ch1528','ch1529','ch1521',
             'ch1530','ch1531','ch1517','ch1404','ch1532','ch1519','ch1406',
             'ch1520','ch1516','ch1523','ch1533','ch1520']
-
+model_list=['Nazca_a0634','Nazca_a0637','Nazca_a0640','Nazca_a0643',
+            'Nazca_a0633','Nazca_a0636','Nazca_a0635',
+            'Nazca_a0631','Nazca_a0630','Nazca_a0642','Nazca_a0641',
+            'Nazca_a0644','Nazca_a0645']
+def nodes_to_elements(xmesh,zmesh):
+    ele_x = (xmesh[:fl.nx-1,:fl.nz-1] + xmesh[1:,:fl.nz-1] + xmesh[1:,1:] + xmesh[:fl.nx-1,1:]) / 4.
+    ele_z = (zmesh[:fl.nx-1,:fl.nz-1] + zmesh[1:,:fl.nz-1] + zmesh[1:,1:] + zmesh[:fl.nx-1,1:]) / 4.
+    return ele_x, ele_z
+def oceanic_slab(frame):
+    phase_oceanic = 3
+    phase_ecolgite = 13
+    phase_oceanic_1 = 17
+    phase_ecolgite_1 = 18
+    x, z = fl.read_mesh(frame)
+    ele_x, ele_z = nodes_to_elements(x,z)
+    phase = fl.read_phase(frame)
+    trench_ind = int(trench_index[frame-1])
+    crust_x = np.zeros(nex)
+    crust_z = np.zeros(nex)
+    for j in range(trench_ind,nex):
+        ind_oceanic = (phase[j,:] == phase_oceanic) + (phase[j,:] == phase_ecolgite)+(phase[j,:] == phase_oceanic_1) + (phase[j,:] == phase_ecolgite_1)
+        if True in ind_oceanic:
+            kk = ele_z[j,ind_oceanic]
+            xx = ele_x[j,ind_oceanic]
+            if len(kk[kk<-15])==0:
+                continue
+            crust_x[j] = np.max(xx[kk<-15])
+            crust_z[j] = np.max(kk[kk<-15])
+    return crust_x,crust_z
+def oceanic_slab2(frame,x,z,phase,trench_index):
+    phase_oceanic = 3
+    phase_ecolgite = 13
+    phase_oceanic_1 = 17
+    phase_ecolgite_1 = 18
+    ele_x, ele_z = nodes_to_elements(x,z)
+    trench_ind = int(trench_index[frame-1])
+    crust_x = np.zeros(len(ele_x))
+    crust_z = np.zeros(len(ele_x))
+    for j in range(trench_ind,len(ele_x)):
+        ind_oceanic = (phase[j,:] == phase_oceanic) + (phase[j,:] == phase_ecolgite)+(phase[j,:] == phase_oceanic_1) + (phase[j,:] == phase_ecolgite_1)
+        if True in ind_oceanic:
+            kk = ele_z[j,ind_oceanic]
+            xx = ele_x[j,ind_oceanic]
+            if len(kk[kk<-15])==0:
+                continue
+            crust_x[j] = np.max(xx[kk<-15])
+            crust_z[j] = np.max(kk[kk<-15])       
+    return crust_x,crust_z
+def temp_elements(temp):
+    ttt = (temp[:fl.nx-1,:fl.nz-1] + temp[1:,:fl.nz-1] + temp[1:,1:] + temp[:fl.nx-1,1:]) / 4.
+    return ttt
+def trench(start_vts=1,model_steps=end):
+    trench_x=np.zeros(end)
+    trench_z=np.zeros(end)
+    trench_index=np.zeros(end)
+    for i in range(start_vts,model_steps):
+        x,z = fl.read_mesh(i)
+        sx,sz=fd.get_topo(x,z)
+        arc_ind,trench_ind=fd.find_trench_index(z)
+        trench_index[i]=trench_ind
+        trench_x[i]=sx[trench_ind]
+        trench_z[i]=sz[trench_ind]
+    return trench_index,trench_x,trench_z
+# trench_index,trench_x,trench_z=trench()
 #model_list = [model]
 depth1 = 80
 depth2 = 130
@@ -71,10 +135,11 @@ for www,model in enumerate(model_list):
         phase = fl.read_phase(i)
         vis=fl.read_visc(i)
         area=fl.read_area(i)
-        ele_x, ele_z = fd.nodes_to_elements(x,z)
-        crust_x,crust_z = fd.oceanic_slab(i,x,z,phase,trench_index)
+        ele_x, ele_z = nodes_to_elements(x,z)
+        # crust_x,crust_z = oceanic_slab(i)
+        crust_x,crust_z = oceanic_slab2(i, x, z, phase, trench_index)
         temp = fl.read_temperature(i)
-        ele_tem = fd.temp_elements(x,z,temp)
+        ele_tem = temp_elements(temp)
         magma = fl.read_fmagma(i)
         wedge_area = np.zeros(nex-int(trench_index[i-1]))
         # fig2,aa=plt.subplots(1,1,figsize=(10,6))
@@ -84,7 +149,7 @@ for www,model in enumerate(model_list):
                 break
             if magma[ii,:].all() == 0:
                 continue  
-            up= (ele_z[ii,:]> crust_z[ii])*(ele_z[ii,:]<-depth1)*(vis[ii,:]<=21)*(magma[ii,:]>=1e-5)
+            up= (ele_z[ii,:]> crust_z[ii])*(ele_z[ii,:]<-depth1)*(vis[ii,:]<=22)*(magma[ii,:]>=1e-5)
             if True in up:
                 wedge_area[ii-int(trench_index[i-1])]=np.mean(area[ii,up]/1e6)
                 areawedge[i-1]+=sum(area[ii,up]/1e6)
@@ -96,7 +161,7 @@ for www,model in enumerate(model_list):
             continue
         temwedge[i-1] = temwedge[i-1]/len(wedge_area[wedge_area>0])
     ccc = fd.moving_window_smooth(channel[channel>0],10)
-    if www >11:
+    if www >5:
         ax3.plot(fl.time[channel>0], ccc,c = color[3],lw=3,label = model)
         ax4.plot(fl.time[channel>0], temwedge[channel>0],c = color[3],lw=3,label = model)
     else:
@@ -119,4 +184,4 @@ ax4.spines['bottom'].set_linewidth(bwith)
 ax4.spines['top'].set_linewidth(bwith)
 ax4.spines['right'].set_linewidth(bwith)
 ax4.spines['left'].set_linewidth(bwith)
-fig3.savefig('/home/jiching/geoflac/figure/'+model_list[0]+'_'+model_list[-1]+'_wedgechannel3_21.png')
+fig3.savefig(savepath+model_list[0]+'_'+model_list[-1]+'_wedgechannel3_21.png')
