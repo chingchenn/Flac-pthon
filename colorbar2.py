78a084f86f4942e400d5e6ecfccfc5ef0d20cb4a
chingchenn <ching602504@gmail.com>
2023-06-03 21:44:46 +0800

    update colorbar code

--- colorbar2.py
+++ colorbar2.py
@@ -23,26 +23,45 @@ path = '/Volumes/SSD500/model/'
 savepath='/home/jiching/geoflac/data/'
 savepath='/Volumes/SSD500/data/'
 figpath='/home/jiching/geoflac/figure/'
-figpath='/Users/ji-chingchen/OneDrive - 國立台灣大學/年會/2022/POSTER/'
-figpath='/Users/chingchen/OneDrive - 國立台灣大學/Thesis_figure/colorbar/'
+# figpath='/Users/ji-chingchen/OneDrive - 國立台灣大學/年會/2022/POSTER/'
+# figpath='/Users/chingchen/OneDrive - 國立台灣大學/Thesis_figure/colorbar/'
+figpath='/Users/chingchen/Desktop/figure/'
 
+
+### viscosity
 fig, axes = plt.subplots(1, 1, figsize=(15, 1))
 fig.subplots_adjust(wspace=2.5)
 labelsize=40
 
-cmap1 = copy.copy(cm.jet)
+cmap1 = copy.copy(cm.jet) # viscosity
+cmap1 = copy.copy(cm.RdBu_r) # Pressure
 norm1 = mcolors.Normalize(vmin=20, vmax=24)
 im1 = cm.ScalarMappable(norm=norm1, cmap=cmap1)
-cbar1 = fig.colorbar(im1, cax=axes,ticks=np.linspace(20, 27, 8),orientation='horizontal')
+cbar1 = fig.colorbar(im1, cax=axes,ticks=np.linspace(20, 24, 8),orientation='horizontal')
 cbar1.ax.tick_params(labelsize=labelsize) 
 # cbar1.set_label('# of contacts', rotation=270,fontsize = 20)
+# fig.savefig(figpath+'viscosity_hor.pdf')
+
+### temperature 
 fig2, axes2 = plt.subplots(1,1, figsize=(15, 1))
 bins = [0,200,400,600,800,1000,1200]
 nbin = len(bins) - 1
-cmap4 = cm.get_cmap('rainbow', nbin)
+cmap4 = cm.get_cmap('RdBu_r', nbin)
 norm4 = mcolors.BoundaryNorm(bins, nbin)
 im4 = cm.ScalarMappable(norm=norm4, cmap=cmap4)
 cbar4 = fig.colorbar(im4, cax=axes2,orientation='horizontal')
 cbar4.ax.tick_params(labelsize=labelsize) 
-# fig.savefig(figpath+'viscosity_hor.pdf')
 # fig2.savefig(figpath+'temperature_hor.pdf')
+
+### pressure
+fig3, axes = plt.subplots(1, 1, figsize=(15, 1))
+fig3.subplots_adjust(wspace=2.5)
+labelsize=40
+cmap1 = copy.copy(cm.jet) # viscosity
+cmap1 = copy.copy(cm.RdBu_r) # Pressure
+norm1 = mcolors.Normalize(vmin=-200, vmax=200)
+im1 = cm.ScalarMappable(norm=norm1, cmap=cmap1)
+cbar1 = fig3.colorbar(im1, cax=axes,ticks=np.linspace(-200, 200, 5),orientation='horizontal')
+cbar1.set_label(label='Pressure (MPa)', size=40)
+cbar1.ax.tick_params(labelsize=labelsize) 
+fig3.savefig(figpath+'pressure_hor.pdf')
