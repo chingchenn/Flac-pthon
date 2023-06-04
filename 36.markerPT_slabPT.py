5e52667a28c5382823f74624f65f9894f7e86c62
chingchenn <ching602504@gmail.com>
2023-06-04 20:37:57 +0800

    update

--- 36.markerPT_slabPT.py
+++ 36.markerPT_slabPT.py
@@ -33,8 +33,9 @@ if Nazca:
     max_dis = 515
     xmin = 300
     xmax = 1000
-    model = 'Ref_Nazca'
-    marker_list = [52238, 51380, 501242, 48622, 60366, 61268, 63076, 62169, 62169, 58564, 59471, 57672, 54078, 52240, 52225, 422137, ]
+    # model = 'Ref_Nazca'
+    model = 'Nazca_a0702'
+    marker_list = [52238]#, 51380, 501242, 48622, 60366, 61268, 63076, 62169, 62169, 58564, 59471, 57672, 54078, 52240, 52225, 422137, ]
 
 
 path='/home/jiching/geoflac/'
@@ -66,7 +67,7 @@ Cocos9
 [248644, 248647, 247521, 634356, 617652, 485634, 598651, 235136, 573900]
 '''
 
-frame = 149
+frame = 120
 labelsize = 20
 bwith = 3
 colors = ["#550A35","#2554C7","#008B8B","#4CC552",
@@ -82,34 +83,7 @@ phase15= matplotlib.colors.ListedColormap(phase_color)
 def temp_elements(temp):
     ttt = (temp[:fl.nx-1,:fl.nz-1] + temp[1:,:fl.nz-1] + temp[1:,1:] + temp[:fl.nx-1,1:]) / 4.
     return ttt
-# ------ read data from model -----
-x, z = fl.read_mesh(frame)
-ele_x, ele_z = flac.elem_coord(x, z)
-temp = fl.read_temperature(frame)
-temp_ele = temp_elements(temp)
-phase = fl.read_phase(frame)
-pressure = fl.read_pres(frame) * -0.1
-time,trench_index, trench_x, trench_z = np.loadtxt(savepath+'trench_for_'+str(model)+'.txt').T
-ind_trench = int(trench_index[frame-1])
 
-# ----- empty array and data -----
-slab_P = np.zeros(len(ele_z))
-slab_T = np.zeros(len(ele_z))
-slab_z = np.zeros(len(ele_z))
-# ----- Start Calculation ------
-for ii,x_ind in enumerate(range(ind_trench-10,len(ele_z))):
-    ind_slab = (ele_z[x_ind,:]> -150)*(ele_z[x_ind,:]< -5)*((phase[x_ind,:] == phase_eclogite) + (phase[x_ind,:] == phase_eclogite_1) + (phase[x_ind,:] == phase_oceanic))
-    if not True in ind_slab:
-        continue
-    if ele_x[x_ind,0]<max_dis:
-        top_slab_index = np.where(ind_slab)[0][-1]
-    elif ele_x[x_ind,0]<max_dis+410:
-        top_slab_index = np.where(ind_slab)[0][-1]
-    else:
-        continue
-    slab_T[x_ind] = temp_ele[x_ind,top_slab_index]
-    slab_z[x_ind] = -ele_z[x_ind,top_slab_index]
-    slab_P[x_ind] = pressure[x_ind,top_slab_index] # GPa
 
            
 fig2,ax2 = plt.subplots(1,1,figsize=(10,10))
@@ -157,25 +131,57 @@ ax2.spines['top'].set_linewidth(bwith)
 ax2.spines['right'].set_linewidth(bwith)
 ax2.spines['left'].set_linewidth(bwith)
 
-axdep.plot(slab_T[slab_T>0],slab_z[slab_T>0],c='green')
-# ax2.scatter(slab_T,slab_P,c='#455E45')
-for kk,idd in enumerate(marker_list):
-    px = np.zeros(end)
-    pz = np.zeros(end)
-    pPre = np.zeros(end)
-    pTemp = np.zeros(end)
-    pmark= np.zeros(end)
-    for ii,qq in enumerate(range(21,150)):
-        mx, mz, age, phase, ID, a1, a2, ntriag= fl.read_markers(qq)
-        temp = fl.read_temperature(qq-10)
-        if len(mx[ID == idd])==0:
+for frame in range(end-,end):
+    # ------ read data from model -----
+    x, z = fl.read_mesh(frame)
+    ele_x, ele_z = flac.elem_coord(x, z)
+    temp = fl.read_temperature(frame)
+    temp_ele = temp_elements(temp)
+    phase = fl.read_phase(frame)
+    pressure = fl.read_pres(frame) * -0.1
+    time,trench_index, trench_x, trench_z = np.loadtxt(savepath+'trench_for_'+str(model)+'.txt').T
+    ind_trench = int(trench_index[frame-1])
+    
+    # ----- empty array and data -----
+    slab_P = np.zeros(len(ele_z))
+    slab_T = np.zeros(len(ele_z))
+    slab_z = np.zeros(len(ele_z))
+    # ----- Start Calculation ------
+    for ii,x_ind in enumerate(range(ind_trench-10,len(ele_z))):
+        ind_slab = (ele_z[x_ind,:]> -150)*(ele_z[x_ind,:]< -5)*((phase[x_ind,:] == phase_eclogite) + (phase[x_ind,:] == phase_eclogite_1) + (phase[x_ind,:] == phase_oceanic))
+        if not True in ind_slab:
             continue
-        px[ii] = mx[ID == idd]
-        pz[ii] = mz[ID == idd]
-        pmark[ii] = phase[ID == idd]
-        pPre[ii] = 3000*10*(-pz[ii])*1000/1e9
-        pTemp[ii] = flac.marker_interpolate_node(ntriag[ID==idd], a1[ID==idd], a2[ID==idd], fl.nz, temp)
-        pTemp[ii] = pTemp[ii] - 0.4*pz[ii]
-    # ax2.scatter(pTemp[pTemp>0],pPre[pTemp>0],c=pmark[pTemp>0],cmap = phase15,s = 40,vmin = 1,vmax = 20)
-    # axdep.scatter(pTemp[pTemp>0],-pz[pTemp>0],c=pmark[pTemp>0],cmap = phase15,s = 40,vmin = 1,vmax = 20)
-    ax2.plot(pTemp[pTemp>0],pPre[pTemp>0],c='purple')
+        if ele_x[x_ind,0]<max_dis:
+            top_slab_index = np.where(ind_slab)[0][-1]
+        elif ele_x[x_ind,0]<max_dis+410:
+            top_slab_index = np.where(ind_slab)[0][-1]
+        else:
+            continue
+        slab_T[x_ind] = temp_ele[x_ind,top_slab_index]
+        slab_z[x_ind] = -ele_z[x_ind,top_slab_index]
+        slab_P[x_ind] = pressure[x_ind,top_slab_index] # GPa
+    slab_z = fd.moving_window_smooth(slab_z, 1)
+    axdep.plot(slab_T[slab_T>0],slab_z[slab_T>0],c='green')
+
+
+# # ax2.scatter(slab_T,slab_P,c='#455E45')
+# for kk,idd in enumerate(marker_list):
+#     px = np.zeros(end)
+#     pz = np.zeros(end)
+#     pPre = np.zeros(end)
+#     pTemp = np.zeros(end)
+#     pmark= np.zeros(end)
+#     for ii,qq in enumerate(range(21,150)):
+#         mx, mz, age, phase, ID, a1, a2, ntriag= fl.read_markers(qq)
+#         temp = fl.read_temperature(qq)
+#         if len(mx[ID == idd])==0:
+#             continue
+#         px[ii] = mx[ID == idd]
+#         pz[ii] = mz[ID == idd]
+#         pmark[ii] = phase[ID == idd]
+#         pPre[ii] = 3300*10*(-pz[ii])*1000/1e9
+#         pTemp[ii] = flac.marker_interpolate_node(ntriag[ID==idd], a1[ID==idd], a2[ID==idd], fl.nz, temp)
+#         pTemp[ii] = pTemp[ii] - 0.4*pz[ii]
+#     # ax2.scatter(pTemp[pTemp>0],pPre[pTemp>0],c=pmark[pTemp>0],cmap = phase15,s = 40,vmin = 1,vmax = 20)
+#     # axdep.scatter(pTemp[pTemp>0],-pz[pTemp>0],c=pmark[pTemp>0],cmap = phase15,s = 40,vmin = 1,vmax = 20)
+#     ax2.plot(pTemp[pTemp>0]+0,pPre[pTemp>0],c='purple')
